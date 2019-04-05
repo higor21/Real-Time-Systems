337aec3d40115c6d271a867e4bad9dcd77b6a1f6
higor21 <felype.cet.15@outlook.com>
2019-04-04 21:46:04 -0300

    commit with erro 'double free'

--- projects/remember_button_sequence_game.cpp
+++ projects/remember_button_sequence_game.cpp
@@ -6,7 +6,9 @@
 #include <cstring>
 #include <sys/time.h>
 #include <cstdlib> // rand()
+#include <time.h>
 #include <vector>
+#include <iostream>
 
 #define MAX_STR 255
 #define NUM_BUTTONS 3
@@ -16,7 +18,7 @@
 using namespace BlackLib;
 	
 // change the display to show the number 'number'
-void set_display(std::vector<BlackGPIO> &display_obj, unsigned int number){
+void set_display(BlackGPIO display_obj[], unsigned int number){
 	vector<int> display;
 
     switch(number) {
@@ -60,8 +62,8 @@ void set_display(std::vector<BlackGPIO> &display_obj, unsigned int number){
     }
 }
 
-void set_leds(std::vector<BlackGPIO> leds, int led /* 0, 1, 2 ou -1*/){
-	for (unsigned i = 0; i < leds.size(); ++i){
+void set_leds(BlackGPIO leds[], int led /* 0, 1, 2 ou -1*/){
+	for (unsigned i = 0; i < NUM_LEDS; ++i){
 		leds[i].setValue((i != (unsigned) led || led == -1) ? low : high);
 	}
 }
@@ -81,14 +83,6 @@ bool verify_if_pressed(BlackGPIO button, BlackGPIO led){
     return false;
 }
 
-void initialize_gpio(/*std::vector<BlackGPIO> itens*/BlackGPIO itens[], int ports[], int n_ports, char c){
-    for(int i=0; i<n_ports; i++){
-    	printf("%d ** %d\n", i, ports[i]);
-    	itens[i] = BlackGPIO((gpioName)ports[i], (c == 'i') ? input : output);
-    	//itens.push_back(BlackGPIO((gpioName)ports[i], (c == 'i') ? input : output));
-    }
-}
-
 bool verify_hit(int r_vet[], int c_vet[], int t){
 	for (int i = 0; i < t; ++i)
 		if (c_vet[i] != r_vet[i]) return false;
@@ -105,22 +99,17 @@ int main(){
     int period_pwm = 1000000;
 	analogLed.setState(statePwm::run);
 	analogLed.setPeriod(period_pwm);
-
-    int display_ports[] = {GPIO_38, GPIO_39, GPIO_34, GPIO_35, GPIO_66, GPIO_67, GPIO_68, GPIO_69};
-    int input_ports[] = {GPIO_38, GPIO_39, GPIO_67};
-    int output_ports[] = {GPIO_63, GPIO_65, GPIO_46};
 	
     // display
-    std::vector<BlackGPIO> displayLed;
+    BlackGPIO displayLed[] = {
+    	BlackGPIO(GPIO_10, output), BlackGPIO(GPIO_9, output), BlackGPIO(GPIO_8, output),
+    	BlackGPIO(GPIO_78, output), BlackGPIO(GPIO_76, output), BlackGPIO(GPIO_74, output),
+    	BlackGPIO(GPIO_72, output), BlackGPIO(GPIO_70, output)
+    };
 	// buttons
-    BlackGPIO buttons[NUM_BUTTONS];
+    BlackGPIO buttons[] = {BlackGPIO(GPIO_38, input), BlackGPIO(GPIO_39, input), BlackGPIO(GPIO_67, input)};
     // leds
-    std::vector<BlackGPIO> leds;
-
-    //buttons.resize(NUM_BUTTONS);
-	initialize_gpio(buttons, input_ports, NUM_BUTTONS, 'i');
-	//initialize_gpio(leds, output_ports, NUM_LEDS, 'o');
-	//initialize_gpio(displayLed, display_ports, NUM_LEDS_DISPLAY, 'o');
+    BlackGPIO leds[] = {BlackGPIO(GPIO_26, output), BlackGPIO(GPIO_65, output), BlackGPIO(GPIO_46, output)};
 
     // user informations
     unsigned int score = 4;
@@ -134,10 +123,13 @@ int main(){
     int real_sequence[15]; // 0, 1, 2 ou -1 (todas apagadas)
     int current_led;
     
-	struct timeval time_init, time_finish;
-    int real_time_of_player;
+    // time
+	//struct timeval time_init, time_finish;
+    //int real_time_of_player;
+
+    time_t time_init, time_finish, real_time_of_player;
 	int cont_sequence_number;
-	int not_pressed;
+	//int not_pressed;
 
 	while(true){
 		
@@ -149,48 +141,61 @@ int main(){
         printf("Start in 3 seconds ...");
         sleep(3);
 
-        set_display(displayLed, 4);
-        while(score < 10 || score > 0){
+        //set_display(displayLed, 4);
+        while(score < 10 && score > 0){
             // informar situação do jogador
-
             // mostrar pisca-pisca de acordo com o tempo
+            //printf("score: %d", score);
+            std::cout<<"score: " << score << std::endl;
             cont_sequence_number = 0;
             time_to_show = dificult*5;
-            while(time_to_show > 0){
+            
+	    while(time_to_show > 0){
                 current_led = rand()%3;
                 correct_sequence[cont_sequence_number++] = current_led;
-                set_leds(leds, current_led);
+		//printf("%d ", correct_sequence[cont_sequence_number - 1]);
+		set_leds(leds, current_led);
                 usleep(500000); // dorme 500ms para mostrar um led aceso
                 time_to_show -= 1;
+                std::cout << "TIME_TO_SHOW: " << time_to_show << endl;
             }
             
-            printf("Apos 3 segundos o jogador porderá responder ...");
+            printf("Apos 3 segundos o jogador porderá responder ...\n");
             sleep(3);
 
             // informa quanto tempo o jogador tem para responder 
             time_to_answer = adcPtm.getPercentValue()*15/100.0 - dificult*3; // máximo 12s e mínimo 6s para resposta
-            printf("%s tem %.2f segundos para responder", name, time_to_answer);
+            printf("\n%s, vc tem %.2f segundos para responder", name, time_to_answer);
             
             // pegando a sequencia do jogador
             real_time_of_player = 0; // [T] = ms
             cont_sequence_number = 0;
             while(time_to_answer >= real_time_of_player/1000.0 || cont_sequence_number >= (int) dificult*5){
-                gettimeofday(&time_init, NULL);
-                
-                not_pressed = 0;
+				std::cout<<"\ntime: "<<real_time_of_player;
+				time_init = time(NULL);
+                //gettimeofday(&time_init, NULL);
+                //not_pressed = 0;
+                printf("pass 1"); 
                 for (int i = 0; i < NUM_BUTTONS; ++i){
                 	if (verify_if_pressed(buttons[i], leds[i])){
                 		real_sequence[cont_sequence_number++] = i;
                 	}
                 }
-                if(not_pressed == 3) real_sequence[cont_sequence_number++] = -1;
+                //if(not_pressed == 3) real_sequence[cont_sequence_number++] = -1;
+                printf("pass 2"); 
 
-                gettimeofday(&time_finish, NULL);
-                real_time_of_player += calc_time_ms(time_init, time_finish); // acrescenta tempo de resposta
+                //gettimeofday(&time_finish, NULL);
+                time_finish = time(NULL);
+                printf("pass 3"); 
+                real_time_of_player += difftime(time_finish,time_init);
+                //real_time_of_player += calc_time_ms(time_init, time_finish); // acrescenta tempo de resposta	    
+                printf("pass 4"); 
             }
 
             if(verify_hit(real_sequence, correct_sequence, (int) dificult*5))
             	set_display(displayLed, ++score);
+	    	else
+				--score;
         }
         (score == 10) ? printf("Parabens!!\n") : printf("Tente novamente, nao foi dessa vez!\n");
     }
