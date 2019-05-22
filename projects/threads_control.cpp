1b85fb9f591ec384bf60c1a8101f7fd79cd838a6
higorcarvalho <felype.cet.15@gmail.com>
2019-05-22 14:07:14 -0300

    update a project of RTS

--- projects/threads_control.cpp
+++ projects/threads_control.cpp
@@ -1,34 +1,25 @@
 #include "../../BlackGPIO/BlackGPIO.h"
 #include "../../ADC/Adc.h"
-#include "../../PWM/PWM.h"
 #include <unistd.h>
 #include <pthread.h>
 #include <sys/resource.h>
 
-#define PERIODO 500000000U
-
-using namespace std;
 using namespace BlackLib;
 
-int prioridade[2] = {0, 0};
+int priorities[2] = {1, 1};
 pthread_mutex_t mutex[2] = {PTHREAD_MUTEX_INITIALIZER, PTHREAD_MUTEX_INITIALIZER};
 
-
-struct Parametros
+struct Params
 {
     BlackGPIO *led;
-    int tarefaID;
+    int taskID;
     
-    Parametros(int t, BlackGPIO *l)
-    {
-        led = l;
-        tarefaID = t;
-    }
+    Params(int id, BlackGPIO *led): taskID(id), led(led) {}
 };
 
-void carga(int N = 4000);
+void weight(int);
 
-void* tarefa(void* params);
+void* task(void*);
 
 int main()
 {    
@@ -37,53 +28,52 @@ int main()
     ADC pot1 = ADC(AIN1);
     BlackGPIO led0 = BlackGPIO(GPIO_48, output);
     BlackGPIO led1 = BlackGPIO(GPIO_51, output);    
-    struct Parametros params_thread0(0, &led0);
-    struct Parametros params_thread1(1, &led1);  
+    struct Params params_thread0(0, &led0);
+    struct Params params_thread1(1, &led1);  
     
-    pthread_create(&thread0, NULL, tarefa, &params_thread0);
-    pthread_create(&thread1, NULL, tarefa, &params_thread1);
+    pthread_create(&thread0, NULL, task, &params_thread0);
+    pthread_create(&thread1, NULL, task, &params_thread1);
     
-    while(1)
+    while(true)
     {
         pthread_mutex_lock(&mutex[0]);
-        prioridade[0] = 19*pot0.getPercentValue()/100.0;
+        priorities[0] = (int) 19*pot0.getPercentValue()/100.0;
         pthread_mutex_unlock(&mutex[0]);
         
         pthread_mutex_lock(&mutex[1]);
-        prioridade[1] = 19*pot1.getPercentValue()/100.0;
+        priorities[1] = (int) 19*pot1.getPercentValue()/100.0;
         pthread_mutex_unlock(&mutex[1]);
         
         sleep(1);
     }
 }
 
-void carga(int N = 4000)
+void weight(int n = 4000)
 {
-    int i, j;
-    double soma = 0.0;
-    for(i = 0; i < N; i++)
+    double sum = 0.0;
+    for(int i = 0; i < n; i++)
     {
-        for(j = 0; j < N; j++)
+        for(int j = 0; j < n; j++)
         {
-            soma += (double) 0.9*0.9*(1.0/3) - 0.27;
+            sum += (double) 0.9*0.9*(1.0/3) - 0.27;
         }
     }
 }
 
-void* tarefa(void* params)
+void* task(void* params)
 {
-    struct Parametros *parametro = (struct Parametros*)params;
+    struct Params *P = (struct Params*) params;
     while(true)
     {
-        pthread_mutex_lock(&mutex[parametro->tarefaID]);
-        setpriority(PRIO_PROCESS, 0, prioridade[parametro->tarefaID]); 
-        pthread_mutex_unlock(&mutex[parametro->tarefaID]);
+        pthread_mutex_lock(&mutex[P->taskID]);
+        setpriority(PRIO_PROCESS, 0, priorities[P->taskID]); 
+        pthread_mutex_unlock(&mutex[P->taskID]);
         
-        cout << "Prioridade Tarefa " << parametro->tarefaID << ": " << getpriority(PRIO_PROCESS, 0) << endl;
+        std::cout << "Priority of Task " << P->taskID << ": " << getpriority(PRIO_PROCESS, 0) << std::endl;
         
-        parametro->led->setValue(high);
-        carga(4000);
-        parametro->led->setValue(low);
-        carga(4000);
+        P->led->setValue(high);
+        weight(4000);
+        P->led->setValue(low);
+        weight(4000);
     }
 }
