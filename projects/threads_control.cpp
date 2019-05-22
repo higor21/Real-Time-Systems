d484ec8b5dbeb70f1cabc44f4549c2152df0abf7
higor21 <felype.cet.15@outlook.com>
2019-05-22 11:45:48 -0300

    upload of RTS's project to edit yet

--- projects/threads_control.cpp
+++ projects/threads_control.cpp
@@ -1,106 +1,89 @@
-/*
- *  thread3.cpp
- *  teste
- *
- *  Created by Luiz Affonso on 04/09/13.
- *  Copyright 2013 __MyCompanyName__. All rights reserved.
- *
- */
-
-// programa thread3.cpp
-// Programa que executar duas threads simultaneamente: main () e thread_function() 
-// Para compilá-lo utilise: g++ -o thread3 thread3.cpp -lpthread
-
-//#include "thread3.h"
-
-#include <stdio.h>
+#include "../../BlackGPIO/BlackGPIO.h"
+#include "../../ADC/Adc.h"
+#include "../../PWM/PWM.h"
 #include <unistd.h>
-#include <stdlib.h>
 #include <pthread.h>
-//#include "BlackGPIO/BlackGPIO.h"
-//#include "PWM/PWM.h"
-//#include "ADC/Adc.h"
-#include <iostream>
-#include <cmath>
-#include <sched.h>
+#include <sys/resource.h>
 
-//using namespace BlackLib;
+#define PERIODO 500000000U
 
-void *thread_function(void *arg);
-void carga(int k);
+using namespace std;
+using namespace BlackLib;
 
-int main() {
+int prioridade[2] = {0, 0};
+pthread_mutex_t mutex[2] = {PTHREAD_MUTEX_INITIALIZER, PTHREAD_MUTEX_INITIALIZER};
 
-    /*ADC adc1(AINx::AIN0); // potentiometer 
-    ADC adc2(AINx::AIN1); // potentiometer 
-    BlackGPIO led_1;
-    BlackGPIO led_2;*/
 
-    char message_t1[] = "1 1 1";
-    char message_t2[] = "2 2 2";
-
-    //pthread_t a_thread;
-    int res_task_1, res_task_2;
-    pthread_t task_1, task_2;
-
-    struct sched_param params_task_1;
-    struct sched_param params_task_2;
-
-    res_task_1 = pthread_create(&task_1, NULL, thread_function, (void*) message_t1);
-    if (res_task_1 != 0) {
-        perror("task_1 creation failed");
-        exit(EXIT_FAILURE);
+struct Parametros
+{
+    BlackGPIO *led;
+    int tarefaID;
+    
+    Parametros(int t, BlackGPIO *l)
+    {
+        led = l;
+        tarefaID = t;
     }
-    res_task_2 = pthread_create(&task_1, NULL, thread_function, (void*) message_t2);
-    if (res_task_2 != 0) {
-        perror("task_2 creation failed");
-        exit(EXIT_FAILURE);
-    }
-    //void *thread_result;
-    //int print_count1 = 0;   
-    while(true){
-        params_task_1.sched_priority = sched_get_priority_max(SCHED_FIFO)-2;
-        params_task_2.sched_priority = sched_get_priority_max(SCHED_FIFO);
+};
 
-        std::cout<<"Prioridade da Tarefa 1: "<<params_task_1.sched_priority<<std::endl;
-        std::cout<<"Prioridade da Tarefa 2: "<<params_task_2.sched_priority<<std::endl;
+void carga(int N = 4000);
 
-        res_task_1 = pthread_setschedparam(task_1, SCHED_FIFO, &params_task_1);
-        if (res_task_1 != 0) {
-            std::cout << "A configuração da prioridade da thread 1 falhou" << std::endl;
-            exit(EXIT_FAILURE);     
-        }
+void* tarefa(void* params);
 
-        res_task_2 = pthread_setschedparam(task_2, SCHED_FIFO, &params_task_2);
-        if (res_task_2 != 0) {
-            std::cout << "A configuração da prioridade da thread 2 falhou" << std::endl;
-            exit(EXIT_FAILURE);
-        }
-
+int main()
+{    
+    pthread_t thread0, thread1;
+    ADC pot0 = ADC(AIN0);
+    ADC pot1 = ADC(AIN1);
+    BlackGPIO led0 = BlackGPIO(GPIO_48, output);
+    BlackGPIO led1 = BlackGPIO(GPIO_51, output);    
+    struct Parametros params_thread0(0, &led0);
+    struct Parametros params_thread1(1, &led1);  
+    
+    pthread_create(&thread0, NULL, tarefa, &params_thread0);
+    pthread_create(&thread1, NULL, tarefa, &params_thread1);
+    
+    while(1)
+    {
+        pthread_mutex_lock(&mutex[0]);
+        prioridade[0] = 19*pot0.getPercentValue()/100.0;
+        pthread_mutex_unlock(&mutex[0]);
+        
+        pthread_mutex_lock(&mutex[1]);
+        prioridade[1] = 19*pot1.getPercentValue()/100.0;
+        pthread_mutex_unlock(&mutex[1]);
+        
         sleep(1);
     }
-    
-    exit(EXIT_SUCCESS);
 }
 
-void *thread_function(/*BlackGPIO* led*/void *arg) {
-    while(true) {
-        printf("THREAD--> thread_function está rodando. O argumento foi %s\n", (char *)arg);
-        //led.setValue(high);
-        carga(1000);
-        //led.setValue(low);
-        carga(1000);
+void carga(int N = 4000)
+{
+    int i, j;
+    double soma = 0.0;
+    for(i = 0; i < N; i++)
+    {
+        for(j = 0; j < N; j++)
+        {
+            soma += (double) 0.9*0.9*(1.0/3) - 0.27;
+        }
     }
 }
 
-
-// função para simular carregamento da CPU
-void carga(int k){
-    float f = 0.999999;
-    for(int i=0; i<k; i++){
-        f = f*f*f*f*f;
-        f = 1.56;
-        for(int j=0; j<k; j++)
-	     f = sin(f)*sin(f)*f*f*f;
+void* tarefa(void* params)
+{
+    struct Parametros *parametro = (struct Parametros*)params;
+    while(true)
+    {
+        pthread_mutex_lock(&mutex[parametro->tarefaID]);
+        setpriority(PRIO_PROCESS, 0, prioridade[parametro->tarefaID]); 
+        pthread_mutex_unlock(&mutex[parametro->tarefaID]);
+        
+        cout << "Prioridade Tarefa " << parametro->tarefaID << ": " << getpriority(PRIO_PROCESS, 0) << endl;
+        
+        parametro->led->setValue(high);
+        carga(4000);
+        parametro->led->setValue(low);
+        carga(4000);
     }
 }
